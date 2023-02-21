75f26a72e37609bf44040c3ac5aa4304d037b8b1
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-02-21 22:06:53 +0900

    UserManager created

--- server/core/models.py
+++ server/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+   use_in_migrations = True
+   
+   def create_user(self, email, password):
+       if not email:
+           raise ValueError('email does not exist')
+       if not password:
+           raise ValueError('password does not exist')
+
+       user = self.model(
+           email=self.normalize_email(email),
+       )
+       user.set_password(password)
+       user.save(using=self._db)
+       return user
+
+   def create_superuser(self, email, password):
+       user = self.create_user(email = self.normalize_email(email), password=password)
+       user.is_admin = True
+       user.is_superuser = True
+       user.save(using=self._db)
+       return user
