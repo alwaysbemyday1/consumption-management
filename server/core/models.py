80c9ddce729634636c586857071918eaeab2b78f
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-02-22 01:16:55 +0900

    model_User created

--- server/core/models.py
+++ server/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -24,3 +24,20 @@ class UserManager(BaseUserManager):
        user.is_superuser = True
        user.save(using=self._db)
        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+   objects = UserManager()
+   
+   email = models.EmailField(max_length=255, unique=True)
+   is_active = models.BooleanField(default=True)
+   is_admin = models.BooleanField(default=False)
+
+   USERNAME_FIELD = 'email'
+   REQUIRED_FIELDS = []
+
+   def __str__(self):
+       return self.email
+
+   @property
+   def is_staff(self):
+       return self.is_admin
