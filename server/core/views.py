3fe51515fcd9d772cc6260e7bf383ee5952d82a6
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-02-22 20:47:51 +0900

    signup connected with JWT

--- server/core/views.py
+++ server/core/views.py
@@ -1,10 +1,30 @@
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.viewsets import ModelViewSet
 
 from .models import User
 from .serializers import UserSerializer
 
-# Create your views here.
+
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save(request)
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh = str(token)
+            access = str(token.access_token)
+
+            data = {
+                'user': serializer.data,
+                'access': access,
+                'refresh': refresh
+            }
+            return Response(data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
