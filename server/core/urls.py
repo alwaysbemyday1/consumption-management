5986c7939377e384e543ceb1adac5ea2b4bdc7bc
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2023-02-24 12:41:16 +0900

    re-instructured veiwsets

--- server/core/urls.py
+++ server/core/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from . import views
+from core.views.ledgerviewset import LedgerViewSet
+from core.views.userviewset import UserViewSet
 
 app_name = 'core'
 
 router = DefaultRouter()
-router.register('users', views.UserViewSet, basename='user')
-router.register('ledgers', views.LedgerViewSet, basename='ledger')
+router.register('users', UserViewSet, basename='user')
+router.register('ledgers', LedgerViewSet, basename='ledger')
 
 urlpatterns = [
     path('api/v0/', include(router.urls)),    
--- server/core/views/ledgerviewset.py
+++ server/core/views/ledgerviewset.py
@@ -1,17 +1,13 @@
-from django.contrib.auth import authenticate
 from django.core import signing
 from django.core.exceptions import SuspiciousOperation
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.viewsets import ModelViewSet
 
-from .models import User, Ledger
-from .serializers import UserSerializer, LedgerSerializer
+from core.models import Ledger
+from core.serializers import LedgerSerializer
 
 class LedgerViewSet(ModelViewSet):
     queryset = Ledger.objects.all()
@@ -90,73 +86,3 @@ class LedgerViewSet(ModelViewSet):
             'result': f'{decoded_data}'
         }
         return Response(data, status=status.HTTP_200_OK)
-
-
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def get_permissions(self):
-        if self.action == 'login' or self.action == 'create':
-            return [AllowAny(), ]
-        return [IsAuthenticated(), ]
-
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save(request)
-            token = TokenObtainPairSerializer.get_token(user)
-            refresh_token = str(token)
-            access_token = str(token.access_token)
-
-            data = {
-                'message': 'Signup Success',
-                'registered_user': serializer.data,
-                'token': {
-                    'access': access_token,
-                    'refresh': refresh_token,
-                },
-            }
-            return Response(data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    
-    @action(methods=['post'], detail=False)
-    def login(self, request):
-        serializer = self.serializer_class(data=request.data)
-
-        user = authenticate(
-            email=request.data.get('email'),
-            password=request.data.get('password')
-        )
-        if user:
-            serializer = UserSerializer(user)
-            token = TokenObtainPairSerializer.get_token(user)
-            refresh_token = str(token)
-            access_token = str(token.access_token)
-            data = {
-                'message': 'Login Success',
-                'logined_user': serializer.data,
-                'token': {
-                    'access': access_token,
-                    'refresh': refresh_token,
-                },
-            }
-            return Response(data, status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        
-    @action(methods=['post'], detail=False)
-    def logout(self, request):
-        try:
-            refresh_token = request.data["refresh_token"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            data = {'message': 'Logout Success'}
-            return Response(data, status=status.HTTP_200_OK)
-        except Exception as error:
-            data = {
-                'message' : 'Logout Failed',
-                'error': str(error)
-            }
-            return Response(data, status=status.HTTP_400_BAD_REQUEST)
